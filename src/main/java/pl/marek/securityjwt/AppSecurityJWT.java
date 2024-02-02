63d4f8dea2ca854e703c1d88475bbed3de38d601
marek <marek@noreply.com>
2024-02-02 19:22:38 +0100

    Initial commit

---
+++ src/main/java/pl/marek/securityjwt/AppSecurityJWT.java
@@ -0,0 +1,11 @@
+package pl.marek.securityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityJWT {
+    public static void main(String[] args) {
+        SpringApplication.run(AppSecurityJWT.class, args);
+    }
+}
